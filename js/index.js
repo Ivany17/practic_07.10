008032c0a70f439de5ed5bdf87b78b22429850f8
Yaremko Ivan <yaremich98@gmail.com>
2021-10-07 11:11:25 +0300

    Add prototype

--- js/index.js
+++ js/index.js
@@ -1,74 +1,91 @@
-// function toString(){
-//     return`${this.firstName} ${this.lastName}`;
-// }
-
-// const animal = {
-//     go: function(){
-//         return `${this.firstName} going`;
+// const rabbit = {
+//     color: 'grey',
+//     jump: function(){
+//         return 'I\'m jumpimg';
 //     }
-// }
+// };
 
-// const man = {
-//     eat: function(){
-//         return `${this.firstName} eating`;
+// const rabbitDom = {
+//     name: 'Georgiy',
+//     pet: function(){
+//         return `${this.name} says: pet me`;
 //     }
-// }
-// man.__proto__ = animal;
-// const student1 = {
-//     id: 1,
-//     firstName: 'Ivan',
-//     lastName: 'Yaremko',
-//     age: 22,
-//     isMale: true,
-//     toString: toString, //same with 20 string
-// };
-// console.log(student1.toString());
-// student1.__proto__ = man;
-// console.log(student1.eat());
-
-// const student2 = {
-//     id: 2,
-//     firstName: 'Andrey',
-//     lastName: 'Rudnitskiy',
-//     age: 25,
-//     isMale: true,
-//     toString, //same with 10 string
-// };
-// console.log(student2.toString());
-// student2.__proto__ = man;
-// console.log(student2.eat());
-
-// const student3 = {
-//     id: 3,
-//     firstName: 'Ann',
-//     lastName: 'Lezedova',
-//     age: 20,
-//     isMale: false,
 // };
 
-const rabbit = {
-    color: 'grey',
-    jump: function(){
-        return 'I\'m jumpimg';
+// const rabbitMag = {
+//     talk: function(talking){
+//         return `${this.name} says: ${talking}`;
+//     }
+// };
+
+// rabbitDom.__proto__ = rabbit;
+// rabbitMag.__proto__ = rabbitDom;
+
+// console.log(rabbitMag.jump());
+// console.log(rabbitMag.talk('Hi there'));
+// console.log(rabbitMag.pet());
+
+
+function toString(){
+    return`${this.firstName} ${this.lastName}`;
+}
+
+const animal = {
+    go: function(){
+        return `${this.firstName} going`;
     }
-};
+}
 
-const rabbitDom = {
-    name: 'Georgiy',
-    pet: function(){
-        return `${this.name} says: pet me`;
+const man = {
+    eat: function(){
+        return `${this.firstName} eating`;
     }
-};
+}
+man.__proto__ = animal;
 
-const rabbitMag = {
-    talk: function(talking){
-        return `${this.name} says: ${talking}`;
+
+function StudentPrototype(){
+    this.toString = function(){
+        return `${this.firstName} ${this.lastName}`
     }
+    this.eat = function(){
+        return `${this.firstName} eating`;
+    }
+    this.go = function(){
+        return `${this.firstName} going`;
+    }
+}
+/**
+ * 
+ * @param {string} firstName 
+ * @param {string} lastName 
+ * @param {number} age 
+ * @param {boolean} isMale 
+ * @returns instance Student
+ */
+function Student(firstName, lastName, age, isMale){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.isMale = isMale; 
+    if(!new.target){
+        return new Student(firstName, lastName, age, isMale);
+    }
+}
+
+Student.prototype = new StudentPrototype(); 
+
+const student = Student('Ivan', 'Yaremko', 22, true);
+console.log(student.toString());
+
+const student2 = {
+    id: 2,
+    firstName: 'Ann',
+    lastName: 'Lezedova',
+    age: 20,
+    isMale: false,
+    toString, //same with 10 string
 };
-
-rabbitDom.__proto__ = rabbit;
-rabbitMag.__proto__ = rabbitDom;
-
-console.log(rabbitMag.jump());
-console.log(rabbitMag.talk('Hi there'));
-console.log(rabbitMag.pet());
+console.log(student2.toString());
+student2.__proto__ = man;
+console.log(student2.eat());
