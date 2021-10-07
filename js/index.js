a8f12569f7ddde16238d2d71b31a1d8041364e6e
Yaremko Ivan <yaremich98@gmail.com>
2021-10-07 11:47:25 +0300

    Add ladder

--- js/index.js
+++ js/index.js
@@ -26,66 +26,94 @@
 // console.log(rabbitMag.pet());
 
 
-function toString(){
-    return`${this.firstName} ${this.lastName}`;
-}
+    // function toString(){
+    //     return`${this.firstName} ${this.lastName}`;
+    // }
 
-const animal = {
-    go: function(){
-        return `${this.firstName} going`;
+    // const animal = {
+    //     go: function(){
+    //         return `${this.firstName} going`;
+    //     }
+    // }
+
+    // const man = {
+    //     eat: function(){
+    //         return `${this.firstName} eating`;
+    //     }
+    // }
+    // man.__proto__ = animal;
+
+
+    // function StudentPrototype(){
+    //     this.toString = function(){
+    //         return `${this.firstName} ${this.lastName}`
+    //     }
+    //     this.eat = function(){
+    //         return `${this.firstName} eating`;
+    //     }
+    //     this.go = function(){
+    //         return `${this.firstName} going`;
+    //     }
+    // }
+    // /**
+    //  * 
+    //  * @param {string} firstName 
+    //  * @param {string} lastName 
+    //  * @param {number} age 
+    //  * @param {boolean} isMale 
+    //  * @returns instance Student
+    //  */
+    // function Student(firstName, lastName, age, isMale){
+    //     this.firstName = firstName;
+    //     this.lastName = lastName;
+    //     this.age = age;
+    //     this.isMale = isMale; 
+    //     if(!new.target){
+    //         return new Student(firstName, lastName, age, isMale);
+    //     }
+    // }
+
+    // Student.prototype = new StudentPrototype(); 
+
+    // const student = Student('Ivan', 'Yaremko', 22, true);
+    // console.log(student.toString());
+
+    const student2 = {
+        id: 2,
+        firstName: 'Ann',
+        lastName: 'Lezedova',
+        age: 20,
+        isMale: false,
+        toString, //same with 10 string
+    };
+    console.log(student2.toString());
+    // student2.__proto__ = man;
+    // console.log(student2.eat());
+
+
+function LadderPrototype(){
+    this.up = function(){
+        this.value++;
+        return this;
+    }
+    this.down = function(){
+        this.value--;
+        return this;
+    }
+    this.showStep = function(){
+        return this.value;
     }
 }
 
-const man = {
-    eat: function(){
-        return `${this.firstName} eating`;
-    }
-}
-man.__proto__ = animal;
-
-
-function StudentPrototype(){
-    this.toString = function(){
-        return `${this.firstName} ${this.lastName}`
-    }
-    this.eat = function(){
-        return `${this.firstName} eating`;
-    }
-    this.go = function(){
-        return `${this.firstName} going`;
-    }
-}
-/**
- * 
- * @param {string} firstName 
- * @param {string} lastName 
- * @param {number} age 
- * @param {boolean} isMale 
- * @returns instance Student
- */
-function Student(firstName, lastName, age, isMale){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.isMale = isMale; 
-    if(!new.target){
-        return new Student(firstName, lastName, age, isMale);
-    }
+function Ladder(){
+    this.value = 0;
 }
 
-Student.prototype = new StudentPrototype(); 
+Ladder.prototype = new LadderPrototype(); 
 
-const student = Student('Ivan', 'Yaremko', 22, true);
-console.log(student.toString());
+const ladder1 = new Ladder();
 
-const student2 = {
-    id: 2,
-    firstName: 'Ann',
-    lastName: 'Lezedova',
-    age: 20,
-    isMale: false,
-    toString, //same with 10 string
-};
-console.log(student2.toString());
-student2.__proto__ = man;
-console.log(student2.eat());
+
+const step = ladder1.up().up().down().up().up().up().up().down().showStep();
+
+console.log(step);
